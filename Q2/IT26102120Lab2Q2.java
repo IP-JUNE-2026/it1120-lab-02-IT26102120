d0cc9db84230afef6c20fdf7c03e53dd7600affe
IT26102120 <it26102120@my.sliit.lk>
2026-06-15 21:25:33 +0530

    IT26102120Lab2Q2.java

---
+++ Q2/IT26102120Lab2Q2.java
@@ -0,0 +1,10 @@
+public class IT26102120Lab2Q2 {
+    public static void main(String[] args) {
+		
+        double length = 10.0;
+        double square_perimeter = 4 * length; 
+        double radius = square_perimeter / (2 * Math.PI);
+
+        System.out.println("Radius of the circular fence: " + radius);
+    }
+}
